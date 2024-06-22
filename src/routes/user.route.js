4722f00f492c065be0d38fcd526e725f24e5e473
Dinh Manh <tranmanhproh1@gmail.com>
2024-06-22 12:45:56 +0700

    feat: [UNI] Init redis to limit request per minute

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 const Router = require("express");
 const UserController = require("../controllers/user.controller.js");
-const { authUser } = require("../middlewares/auth.middleware.js");
+const { authUser } = require("../middlewares/auth.middleware");
+const { limitRequest } = require("../middlewares/limiter.middleware");
 
 const router = Router();
 
-router.get("/", authUser, UserController.getAllUsers);
+router.get("/", limitRequest, authUser, UserController.getAllUsers);
 
 module.exports = router;
