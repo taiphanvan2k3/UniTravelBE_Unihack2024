c89d00e9e23d99c5921041274b801f1c4a6b1afe
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-07-27 15:15:35 +0700

    feat: [Uni] Update logic login on 27/07/2024

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,188 +2,191 @@ const authService = require("../services/auth/auth.service.js");
 const { logInfo, logError } = require("../services/logger.service.js");
 
 class AuthController {
-  async registerUser(req, res, next) {
-    logInfo("registerUser", "Start");
-    const { email, password, displayName, role } = req.body;
-    if (!email || !password) {
-      return res.status(422).json({
-        email: "Email is required",
-        password: "Password is required",
-        displayName: "Display name is required",
-        role: "Role is required",
-      });
+    async registerUser(req, res, next) {
+        logInfo("registerUser", "Start");
+        const { email, password, displayName, role } = req.body;
+        if (!email || !password) {
+            return res.status(422).json({
+                email: "Email is required",
+                password: "Password is required",
+                displayName: "Display name is required",
+                role: "Role is required",
+            });
+        }
+
+        try {
+            await authService.registerUser(email, password, displayName, role);
+            logInfo("registerUser", "End");
+            return res.status(201).json({
+                message: "Verification email sent! User created successfully!",
+            });
+        } catch (error) {
+            if (error.message == "Verification email is re-sent") {
+                return res.status(200).json({
+                    message: error.message,
+                });
+            }
+
+            logError("registerUser", error.message);
+            res.status(500).json({
+                message: error.message,
+            });
+            next(error);
+        }
     }
 
-    try {
-      await authService.registerUser(email, password, displayName, role);
-      logInfo("registerUser", "End");
-      return res.status(201).json({
-        message: "Verification email sent! User created successfully!",
-      });
-    } catch (error) {
-      if (error.message == "Verification email is re-sent") {
-        return res.status(200).json({
-          message: error.message,
-        });
-      }
+    async verifyToken(req, res, next) {
+        logInfo("verifyToken", "Start");
+        let { token } = req.body;
+        if (!token) {
+            token = req.cookies?.access_token;
+        }
 
-      logError("registerUser", error.message);
-      res.status(500).json({
-        message: error.message,
-      });
-      next(error);
-    }
-  }
+        if (!token) {
+            logInfo("verifyToken", "End");
+            return res.status(422).json({
+                token: "Token is required",
+            });
+        }
 
-  async verifyToken(req, res, next) {
-    logInfo("verifyToken", "Start");
-    let { token } = req.body;
-    if (!token) {
-      token = req.cookies?.access_token;
-    }
+        try {
+            const userInfo = await authService.verifyToken(token);
+            if (!userInfo) {
+                return res.status(404).json({
+                    message: "User not found!",
+                });
+            }
 
-    if (!token) {
-      logInfo("verifyToken", "End");
-      return res.status(422).json({
-        token: "Token is required",
-      });
-    }
+            res.status(200).json({
+                user: userInfo,
+                token,
+            });
 
-    try {
-      const userInfo = await authService.verifyToken(token);
-      if (!userInfo) {
-        return res.status(404).json({
-          message: "User not found!",
-        });
-      }
+            logInfo("verifyToken", "End");
+        } catch (error) {
+            logError("verifyToken", error.message);
 
-      res.status(200).json({
-        user: userInfo,
-        token,
-      });
-
-      logInfo("verifyToken", "End");
-    } catch (error) {
-      logError("verifyToken", error.message);
-
-      res.status(401).json({
-        type: "token_error",
-        message: "Verify token failed!",
-      });
-      next(error);
-    }
-  }
-
-  async loginUser(req, res, next) {
-    logInfo("loginUser", "Start");
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return res.status(422).json({
-        email: "Email is required",
-        password: "Password is required",
-      });
+            res.status(401).json({
+                type: "token_error",
+                message: "Verify token failed!",
+            });
+            next(error);
+        }
     }
 
-    try {
-      const { token, userInfo } = await authService.loginUser(email, password);
+    async loginUser(req, res, next) {
+        logInfo("loginUser", "Start");
+        const { email, password } = req.body;
 
-      res.cookie("access_token", token, {
-        maxAge: 3600000,
-        path: "/",
-      });
+        if (!email || !password) {
+            return res.status(422).json({
+                email: "Email is required",
+                password: "Password is required",
+            });
+        }
 
-      res.status(200).json({
-        token,
-        user: userInfo,
-      });
+        try {
+            const { token, userInfo } = await authService.loginUser(
+                email,
+                password
+            );
 
-      logInfo("loginUser", "End");
-    } catch (error) {
-      logError("loginUser", error.message);
-      if (error.message === "Email is not verified") {
-        res.status(403).json({
-          type: "email_verification_error",
-          message: error.message,
-        });
-      } else {
-        res.status(401).json({
-          type: "login_error",
-          message: error.message,
-        });
-      }
-      next(error);
-    }
-  }
+            res.cookie("access_token", token, {
+                maxAge: 3600000,
+                path: "/",
+            });
 
-  async signOutUser(req, res, next) {
-    try {
-      logInfo("signOutUser", "Start");
-      const { email } = req.body;
-      const emailFromAccessToken = req.user.email;
+            res.status(200).json({
+                token,
+                user: userInfo,
+            });
 
-      if (email !== emailFromAccessToken) {
-        return res.status(403).json({
-          message: "Email does not match with the token!",
-        });
-      }
-
-      await authService.signOut(req.user.uid);
-      res.clearCookie("access_token");
-      res.status(200).json({
-        message: `user with email ${req.user.email} signed out successfully!`,
-      });
-
-      logInfo("signOutUser", "End");
-    } catch (error) {
-      logError("signOutUser", error.message);
-      next(error);
-    }
-  }
-
-  async resetPassword(req, res) {
-    logInfo("resetPassword", "Start");
-    const { email } = req.body;
-    if (!email) {
-      return res.status(422).json({
-        email: "Email is required",
-      });
+            logInfo("loginUser", "End");
+        } catch (error) {
+            logError("loginUser", error.message);
+            if (error.message === "Email is not verified") {
+                res.status(403).json({
+                    type: "email_verification_error",
+                    message: error.message,
+                });
+            } else {
+                res.status(401).json({
+                    type: "login_error",
+                    message: error.message,
+                });
+            }
+            next(error);
+        }
     }
 
-    try {
-      await authService.resetPassword(email);
-      res.status(200).json({
-        message: "Password reset email sent!",
-      });
-      logInfo("resetPassword", "End");
-    } catch (error) {
-      logError("resetPassword", error.message);
-      next(error);
-    }
-  }
+    async signOutUser(req, res, next) {
+        try {
+            logInfo("signOutUser", "Start");
+            const { email } = req.body;
+            const emailFromAccessToken = req.user.email;
 
-  async checkEmailIsExist(req, res) {
-    logInfo("checkEmailIsExist", "Start");
-    const { email } = req.body;
-    if (!email) {
-      return res.status(422).json({
-        email: "Email is required",
-      });
+            if (email !== emailFromAccessToken) {
+                return res.status(403).json({
+                    message: "Email does not match with the token!",
+                });
+            }
+
+            await authService.signOut(req.user.uid);
+            res.clearCookie("access_token");
+            res.status(200).json({
+                message: `user with email ${req.user.email} signed out successfully!`,
+            });
+
+            logInfo("signOutUser", "End");
+        } catch (error) {
+            logError("signOutUser", error.message);
+            next(error);
+        }
     }
 
-    try {
-      const isExist = await authService.checkEmailIsExist(email);
-      res.status(200).json({
-        isExist,
-      });
-      logInfo("checkEmailIsExist", "End");
-    } catch (error) {
-      res.status(500).json({
-        message: error.message,
-      });
-      logError("checkEmailIsExist", error.message);
+    async resetPassword(req, res) {
+        logInfo("resetPassword", "Start");
+        const { email } = req.body;
+        if (!email) {
+            return res.status(422).json({
+                email: "Email is required",
+            });
+        }
+
+        try {
+            await authService.resetPassword(email);
+            res.status(200).json({
+                message: "Password reset email sent!",
+            });
+            logInfo("resetPassword", "End");
+        } catch (error) {
+            logError("resetPassword", error.message);
+            next(error);
+        }
     }
-  }
+
+    async checkEmailIsExist(req, res) {
+        logInfo("checkEmailIsExist", "Start");
+        const { email } = req.body;
+        if (!email) {
+            return res.status(422).json({
+                email: "Email is required",
+            });
+        }
+
+        try {
+            const isExist = await authService.checkEmailIsExist(email);
+            res.status(200).json({
+                isExist,
+            });
+            logInfo("checkEmailIsExist", "End");
+        } catch (error) {
+            res.status(500).json({
+                message: error.message,
+            });
+            logError("checkEmailIsExist", error.message);
+        }
+    }
 }
 
 module.exports = new AuthController();
--- src/services/user/user-detail.service.js
+++ src/services/user/user-detail.service.js
@@ -6,168 +6,163 @@ const User = require("../../models/user.model");
 const UserVoucher = require("../../models/user-voucher.model");
 
 const convertUserDBToUser = (userDB) => {
-  return {
-    userId: userDB._id,
-    firebaseUserId: userDB.firebaseUserId,
-    email: userDB.email,
-    username: userDB.username,
-    displayName: userDB.displayName,
-    imageUrl: userDB.imageUrl,
-    phoneNumber: userDB.phoneNumber,
-    address: userDB.address,
-    isAdmin: userDB.isAdmin,
-    isVerified: userDB.isVerified,
-    isOnline: userDB.isOnline,
-    roles: userDB.roles,
-    badges: userDB.badges,
-  };
+    return {
+        userId: userDB._id,
+        firebaseUserId: userDB.firebaseUserId,
+        email: userDB.email,
+        username: userDB.username,
+        displayName: userDB.displayName,
+        imageUrl: userDB.imageUrl,
+        phoneNumber: userDB.phoneNumber,
+        address: userDB.address,
+        isAdmin: userDB.isAdmin,
+        isVerified: userDB.isVerified,
+        isOnline: userDB.isOnline,
+        roles: userDB.roles,
+        badges: userDB.badges,
+    };
 };
 
 const findUser = async (type, value) => {
-  try {
-    const user = await User.findOne({ [type]: value });
-    if (user) {
-      return convertUserDBToUser(user);
+    try {
+        const user = await User.findOne({ [type]: value });
+        if (user) {
+            return convertUserDBToUser(user);
+        }
+        return null;
+    } catch (error) {
+        console.log(error.message);
+        throw error;
     }
-    return null;
-  } catch (error) {
-    console.log(error.message);
-    throw error;
-  }
 };
 
 const updateUserOnlineStatus = async (userDB) => {
-  userDB.isOnline = true;
-  userDB.isVerified = true;
-  await userDB.save();
-  return {
-    userId: userDB._id,
-    firebaseUserId: userDB.firebaseUserId,
-    email: userDB.email,
-    username: userDB.username,
-    displayName: userDB.displayName,
-    imageUrl: userDB.imageUrl,
-    phoneNumber: userDB.phoneNumber,
-    address: userDB.address,
-    isAdmin: userDB.isAdmin,
-    isVerified: userDB.isVerified,
-    isOnline: userDB.isOnline,
-    roles: userDB.roles,
-    badges: userDB.badges,
-  };
+    userDB.isOnline = true;
+    userDB.isVerified = true;
+    await userDB.save();
+    return {
+        userId: userDB._id,
+        firebaseUserId: userDB.firebaseUserId,
+        email: userDB.email,
+        username: userDB.username,
+        displayName: userDB.displayName,
+        imageUrl: userDB.imageUrl,
+        phoneNumber: userDB.phoneNumber,
+        address: userDB.address,
+        isAdmin: userDB.isAdmin,
+        isVerified: userDB.isVerified,
+        isOnline: userDB.isOnline,
+        roles: userDB.roles,
+        badges: userDB.badges,
+    };
 };
 
 const createTempUser = async (user) => {
-  let userDB = await User.findOne({ firebaseUserId: user.uid });
-  if (!userDB) {
-    const userInfo = {
-      firebaseUserId: user.uid,
-      email: user.email,
-      username: user.email.split("@")[0],
-      displayName: user.displayName,
-      imageUrl: user.imageUrl ?? "",
-      phoneNumber: "",
-      address: "",
-      isAdmin: false,
-      isVerified: false,
-      isOnline: false,
-      roles: ["traveler"],
-    };
+    let userDB = await User.findOne({ firebaseUserId: user.uid });
+    if (!userDB) {
+        const userInfo = {
+            firebaseUserId: user.uid,
+            email: user.email,
+            username: user.email.split("@")[0],
+            displayName: user.displayName,
+            imageUrl: user.imageUrl ?? "",
+            phoneNumber: "",
+            address: "",
+            isAdmin: false,
+            isVerified: false,
+            isOnline: false,
+            roles: ["traveler"],
+        };
 
-    userDB = new User(userInfo);
-    await userDB.save();
-  }
+        userDB = new User(userInfo);
+        await userDB.save();
+    }
 };
 
 const createOrUpdateUser = async (user) => {
-  try {
-    let userDB = await User.findOne({ firebaseUserId: user.uid }).populate(
-      "badges"
-    );
-    if (userDB) {
-      return await updateUserOnlineStatus(userDB);
-    } else {
-      const userInfo = {
-        firebaseUserId: user.uid,
-        email: user.email,
-        username: user.email.split("@")[0],
-        displayName: user.displayName ?? user.email.split("@")[0],
-        imageUrl: user.photoURL,
-        phoneNumber: user.phoneNumber,
-        address: "",
-        isAdmin: false,
-        isVerified: true,
-        isOnline: true,
-        badges: [],
-      };
+    try {
+        let userDB = await User.findOne({ firebaseUserId: user.uid }).populate(
+            "badges"
+        );
+        if (userDB) {
+            return await updateUserOnlineStatus(userDB);
+        } else {
+            const userInfo = {
+                firebaseUserId: user.uid,
+                email: user.email,
+                username: user.email.split("@")[0],
+                displayName: user.displayName ?? user.email.split("@")[0],
+                imageUrl: user.photoURL,
+                phoneNumber: user.phoneNumber,
+                address: "",
+                isAdmin: false,
+                isVerified: true,
+                isOnline: true,
+                badges: [],
+            };
 
-      userDB = new User(userInfo);
-      await userDB.save();
-      userInfo.userId = userDB._id;
-      return userInfo;
+            userDB = new User(userInfo);
+            await userDB.save();
+            userInfo.userId = userDB._id;
+            return userInfo;
+        }
+    } catch (error) {
+        console.log(error.message);
+        throw error;
     }
-  } catch (error) {
-    console.log(error.message);
-    throw error;
-  }
 };
 
 const updateUser = async (identifier, identifierValue, data) => {
-  try {
-    const result = await User.updateOne(
-      { [identifier]: identifierValue },
-      { $set: data }
-    );
-    if (result.nModified === 0) {
-      throw new Error("Update user failed");
+    try {
+        const result = await User.updateOne(
+            { [identifier]: identifierValue },
+            { $set: data }
+        );
+        if (result.nModified === 0) {
+            throw new Error("Update user failed");
+        }
+    } catch (error) {
+        console.error("Error updating user:", error.message);
+        throw error;
     }
-  } catch (error) {
-    console.error("Error updating user:", error.message);
-    throw error;
-  }
 };
 
 const getVouchers = async (userId) => {
-  try {
-    logInfo("getVouchers", "Start");
-    const currentUserId = appState.context.currentUser.userIdInSystem;
-    const currentUser = await User.findById(currentUserId);
-    if (!currentUser) {
-      throw new Error("404-User not found");
+    try {
+        logInfo("getVouchers", "Start");
+        const currentUser = await User.findById(userId);
+        if (!currentUser) {
+            throw new Error("404-User not found");
+        }
+
+        // Group by voucher theo loại
+        const vouchers = await UserVoucher.find({
+            user: currentUser._id,
+        }).populate("voucher");
+
+        const groupedVouchers = {};
+        vouchers.forEach((userVoucher) => {
+            const voucher = userVoucher.voucher;
+            if (!groupedVouchers[voucher.code]) {
+                groupedVouchers[voucher.code] = [];
+            }
+            groupedVouchers[voucher.code].push({
+                voucher,
+            });
+        });
+
+        logInfo("getVouchers", "End");
+        return groupedVouchers;
+    } catch (error) {
+        logError("getVouchers", error.message);
+        throw error;
     }
-
-    if (userId !== currentUserId.toString()) {
-      throw new Error("400-UserId is not valid");
-    }
-
-    // Group by voucher theo loại
-    const vouchers = await UserVoucher.find({
-      user: currentUser._id,
-    }).populate("voucher");
-
-    const groupedVouchers = {};
-    vouchers.forEach((userVoucher) => {
-      const voucher = userVoucher.voucher;
-      if (!groupedVouchers[voucher.code]) {
-        groupedVouchers[voucher.code] = [];
-      }
-      groupedVouchers[voucher.code].push({
-        voucher,
-      });
-    });
-
-    logInfo("getVouchers", "End");
-    return groupedVouchers;
-  } catch (error) {
-    logError("getVouchers", error.message);
-    throw error;
-  }
 };
 
 module.exports = {
-  findUser,
-  createTempUser,
-  createOrUpdateUser,
-  updateUser,
-  getVouchers,
+    findUser,
+    createTempUser,
+    createOrUpdateUser,
+    updateUser,
+    getVouchers,
 };
